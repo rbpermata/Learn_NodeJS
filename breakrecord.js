e0c0361cba1ec3977b8ce32fed6946af05d3077e
rikyboy <permata.rb@gmail.com>
2018-11-15 20:31:13 +0700

    mark comment breackrecord.js

--- breakrecord.js
+++ breakrecord.js
@@ -1,27 +1,27 @@
-//https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+// //https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
 
-var array = [10,5,20,20,4,5,2,25,1];
+// var array = [10,5,20,20,4,5,2,25,1];
 
-function minmax(array)
-{
-    var min = array[0];
-    var max = array[0];
-    var countmin = 0;
-    var countmax = 0;
+// function minmax(array)
+// {
+//     var min = array[0];
+//     var max = array[0];
+//     var countmin = 0;
+//     var countmax = 0;
 
-    for (var i=0; i<array.length; i++)
-    {
-        if(array[i]>max)
-        {
-            countmax ++; //Menambah counter min value
-            max = array[i];
-        }
-        else if(array[i]<min)
-        {
-            countmin ++;
-            min = array[i];
-        }
-    }
-    console.log(countmax + ' ' + countmin);
-}
-minmax(array);
+//     for (var i=0; i<array.length; i++)
+//     {
+//         if(array[i]>max)
+//         {
+//             countmax ++; //Menambah counter min value
+//             max = array[i];
+//         }
+//         else if(array[i]<min)
+//         {
+//             countmin ++;
+//             min = array[i];
+//         }
+//     }
+//     console.log(countmax + ' ' + countmin);
+// }
+// minmax(array);
